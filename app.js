a90caca16f9f96b9a26a36157cf3ddd40611e5cc
tomek.siudut <tomasz.siudut@giraffestudio.pl>
2022-04-16 16:15:23 +0200

    mongo DB database connected

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require("express");
-const app = express();
-
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 require("dotenv/config");
 
 const api = process.env.API_URL;
+const app = express();
 
-app.get(api + "/products", (req, res) => {
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.get(`${api}/products`, (req, res) => {
   res.send("this is a list of products");
 });
 
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "eshop-database",
+  })
+  .then(() => {
+    console.log("Database connection is ready");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.listen(3000, () => {
-  console.log(api);
   console.log("server started");
 });
