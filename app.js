ae93e5a317b11160919e08c6a0c53fc6e9579c0a
tomek.siudut <tomasz.siudut@giraffestudio.pl>
2022-04-16 17:38:08 +0200

    added new fields to the product schema

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const api = process.env.API_URL;
+
+const app = express();
 require("dotenv/config");
 
-const api = process.env.API_URL;
-const app = express();
+app.use(cors());
+app.options("*", cors());
+
+// Routes
 const productsRouter = require("./routers/products");
 
+// Middlewares
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(`${api}/products`, productsRouter);
 
+// DB connection
 mongoose
   .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
@@ -24,6 +32,7 @@ mongoose
     console.log(err);
   });
 
+// Server
 app.listen(3000, () => {
   console.log("server started");
 });
