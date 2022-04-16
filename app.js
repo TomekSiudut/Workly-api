79a6e7bf7a0c4cb61b9584bb6d329cc9ea13a846
tomek.siudut <tomasz.siudut@giraffestudio.pl>
2022-04-16 15:26:11 +0200

    base express setup

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello api");
+});
+
+app.listen(3000, () => {
+  console.log("server started");
+});
