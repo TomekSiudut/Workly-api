0c5cacac6d291decc6bad99058a657a99dc4e550
tomek.siudut <tomasz.siudut@giraffestudio.pl>
2022-04-16 18:12:30 +0200

    delete method added

--- routers/categories.js
+++ routers/categories.js
@@ -24,4 +24,24 @@ router.post("/", async (req, res) => {
   res.send(category);
 });
 
+router.delete("/:id", (req, res) => {
+  Category.findByIdAndRemove(req.params.id)
+    .then((category) => {
+      if (category) {
+        return res.status(200).json({
+          success: true,
+          message: "the category is deleted",
+        });
+      } else {
+        return res.status(404).json({
+          success: false,
+          message: "category not found",
+        });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
+
 module.exports = router;
