96e84cbb9c59f407b6d13f2d897c4daa7ac4407c
tomek.siudut <tomasz.siudut@giraffestudio.pl>
2022-04-16 20:22:01 +0200

    adding filtering by categories

--- routers/products.js
+++ routers/products.js
@@ -2,9 +2,15 @@ const { Product } = require("../models/product");
 const express = require("express");
 const { Category } = require("../models/category");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find();
+  let filter = {};
+  if (req.query.categories) {
+    filter = { category: req.query.categories.split(",") };
+  }
+
+  let productList = await Product.find(filter).populate("category");
 
   if (!productList) {
     res.status(500).json({ success: false });
@@ -18,6 +24,54 @@ router.get("/:id", async (req, res) => {
     res.status(500).json({ success: false });
   }
   res.send(product);
+});
+
+router.put("/:id", async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(400).send("Invalid product Id");
+  }
+  let category = await Category.findById(req.body.category);
+  if (!category) return res.status(400).send("Invalid Category");
+
+  let product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    },
+    { new: true }
+  );
+
+  if (!product) return res.status(400).send("the product cannot be updated");
+  res.send(product);
+});
+
+router.delete("/:id", (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(400).send("Invalid product Id");
+  }
+  Product.findByIdAndRemove(req.params.id)
+    .then((product) => {
+      if (product) {
+        res.status(200).json({ success: true, message: "product deleted" });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "product not found" });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({ success: false, error: err });
+    });
 });
 
 router.post(`/`, async (req, res) => {
@@ -42,6 +96,28 @@ router.post(`/`, async (req, res) => {
 
   if (!product) return res.status(500).send("The product cannot be created");
   res.send(product);
+});
+
+router.get("/get/count", async (req, res) => {
+  let productCount = await Product.countDocuments();
+  if (!productCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({
+    productCount: productCount,
+  });
+});
+
+router.get("/get/featured/:count", async (req, res) => {
+  let count = req.params.count ? req.params.count : 0;
+  let products = await Product.find({
+    isFeatured: true,
+  }).limit(+count);
+
+  if (!products) {
+    res.status(500).json({ success: false });
+  }
+  res.send(products);
 });
 
 module.exports = router;
