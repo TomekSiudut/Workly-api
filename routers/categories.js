d159744e420c7fcccccda3f92c5c6719be9e15ed
tomek.siudut <tomasz.siudut@giraffestudio.pl>
2022-04-16 18:27:08 +0200

    delete and update method added

--- routers/categories.js
+++ routers/categories.js
@@ -8,7 +8,28 @@ router.get("/", async (req, res) => {
   if (!categoryList) {
     res.status(500).json({ success: false });
   }
-  res.send(categoryList);
+  res.status(200).send(categoryList);
+});
+
+router.get("/:id", async (req, res) => {
+  let category = await Category.findById(req.params.id);
+  if (!category) {
+    res
+      .send(500)
+      .json({ message: "The category with the given ID was not found." });
+  }
+  res.status(200).send(category);
+});
+
+router.put("/:id", async (req, res) => {
+  let category = await Category.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  });
+
+  if (!category) return res.status(400).send("the category cannot be updated");
+  res.send(category);
 });
 
 router.post("/", async (req, res) => {
@@ -20,7 +41,7 @@ router.post("/", async (req, res) => {
 
   category = await category.save();
 
-  if (!category) return res.status(404).send("the category cannot be created");
+  if (!category) return res.status(404).send("The category cannot be created.");
   res.send(category);
 });
 
